7cca349534faaf059a4455a0f8766b472a371a7f
eliran17e <eliran17eli@gmail.com>
2025-03-30 21:53:27 +0300

    Update 5.4.communicating_vessels.py

--- src/5.4.communicating_vessels.py
+++ src/5.4.communicating_vessels.py
@@ -1,38 +1,50 @@
-def interleave(*list):
+"""
+This module provides functions to interleave multiple lists into one list.
+"""
+
+def interleave(*lists):
     """
     Interleave multiple lists into one list.
 
     Parameters:
-    list (list): The lists to be interleaved
+    lists (list): The lists to be interleaved
 
-    Returns the interleaved list
+    Returns:
+    list: The interleaved list
     """
     result = []
-    max_length = max((len(lst) for lst in list), default=0)
+    max_length = max((len(lst) for lst in lists), default=0)
     for i in range(max_length):
-        for iter in list:
-            if i < len(iter):
-                result.append(iter[i])
+        for lst in lists:
+            if i < len(lst):
+                result.append(lst[i])
     return result
-def generator_interleave(*list):
+
+def generator_interleave(*lists):
     """
-    Interleave multiple lists into one list.
+    Interleave multiple lists into one list using a generator.
 
     Parameters:
-    list (list): The lists to be interleaved
+    lists (list): The lists to be interleaved
 
-    Returns the interleaved list
+    Yields:
+    element: The next element in the interleaved list
     """
-    max_length = max((len(lst) for lst in list), default=0)
+    max_length = max((len(lst) for lst in lists), default=0)
     for i in range(max_length):
-        for iter in list:
-            if i < len(iter):
-                yield iter[i]
+        for lst in lists:
+            if i < len(lst):
+                yield lst[i]
+
 def main():
-    list1 = [1, 2, 3,4,5]
+    """
+    Main function to demonstrate the usage of the interleave functions.
+    """
+    list1 = [1, 2, 3, 4, 5]
     list2 = ['a', 'b', 'c']
-    list3 = ['x', 'y', 'z','w']
+    list3 = ['x', 'y', 'z', 'w']
     print(interleave(list1, list2, list3))
     print(list(generator_interleave(list1, list2, list3)))
+
 if __name__ == '__main__':
     main()
