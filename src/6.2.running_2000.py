f9e725d776f12235c2ce3447a456c184432c5194
eliran17e <eliran17eli@gmail.com>
2025-03-30 16:21:11 +0300

    running 2000

---
+++ src/6.2.running_2000.py
@@ -0,0 +1,22 @@
+import time
+def timer(f,*parameters):
+    """
+    A decorator that prints the time a function takes to execute.
+    """
+    start = time.time()
+    f(*parameters)
+    end = time.time()
+    result=end-start
+    return result
+def long_function(list=[1,2,3,4,5]):
+    """
+    A function that simulates a long-running process.
+    """
+    for i in list:
+       time.sleep(i)
+def main():
+
+    result = timer(long_function,[1,2])
+    print(f"Time taken: {result} seconds")
+if __name__ == '__main__':
+    main()
