02af9d8804b1b1ed27e9a58c6ad0a21d32086fd1
eliran17e <eliran17eli@gmail.com>
2025-03-30 21:39:11 +0300

    Update 5.1.thats_the_way.py

--- src/5.1.thats_the_way.py
+++ src/5.1.thats_the_way.py
@@ -1,12 +1,18 @@
+"""
+This module provides a function to list files in a directory that start with a given prefix.
+"""
+
 import os
 
 def thats_the_way(path):
     """
+    List files in the given directory that start with the prefix 'deep'.
 
-      Parameters:
-    path: The directory path
+    Parameters:
+    path (str): The directory path
 
-    returns: list of files that start with the given prefix
+    Returns:
+    list: List of files that start with the given prefix
     """
     matching_folders = []
     try:
@@ -14,12 +20,15 @@ def thats_the_way(path):
             if file.startswith("deep"):
                 matching_folders.append(file)
     except FileNotFoundError:
-        print(f"Directory '{dict}' not found.")
-    except Exception as e:
+        print(f"Directory '{path}' not found.")
+    except OSError as e:
         print(f"An error occurred: {e}")
     return matching_folders
 
 def main():
+    """
+    Main function to list files in the './images' directory that start with the prefix 'deep'.
+    """
     relative_path = "./images"
     full_path = os.path.abspath(relative_path)
     print("Full path to the directory:", full_path)
