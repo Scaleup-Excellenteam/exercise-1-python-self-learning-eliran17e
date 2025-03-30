2ae52a79fe31058e40beebe0194e0936dbba2f4e
eliran17e <eliran17eli@gmail.com>
2025-03-30 17:53:39 +0300

    Update 5.1.thats_the_way.py

--- src/5.1.thats_the_way.py
+++ src/5.1.thats_the_way.py
@@ -1,30 +1,30 @@
 import os
 
-def thats_the_way(dict, file_name):
+def that_the_way( file_name):
     """
 
       Parameters:
-    dict (str): The directory path
     file_name (str): The file name prefix
 
     returns: list of files that start with the given prefix
     """
+    relative_path = "./images"
+    full_path = os.path.abspath(relative_path)
+    print("Full path to the directory:", full_path)
     matching_folders = []
     try:
-        for file in os.listdir(dict):
+        for file in os.listdir(full_path):
             if file.startswith(file_name):
                 matching_folders.append(file)
     except FileNotFoundError:
-        print(f"Directory '{dict}' not found.")
+        print(f"Directory '{full_path}' not found.")
     except Exception as e:
         print(f"An error occurred: {e}")
     return matching_folders
 
 def main():
-    relative_path = "./images"
-    full_path = os.path.abspath(relative_path)
-    print("Full path to the directory:", full_path)
-    print(that_the_way(full_path, "deep"))
+
+    print(that_the_way( "deep"))
 
 if __name__ == '__main__':
     main()
