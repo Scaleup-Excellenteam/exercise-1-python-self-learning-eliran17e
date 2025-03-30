13db984b9145f174cce3cfe0a786cbe49b103151
eliran17e <eliran17eli@gmail.com>
2025-03-30 22:00:28 +0300

    Update 6.5.group_by.py

--- src/6.5.group_by.py
+++ src/6.5.group_by.py
@@ -1,14 +1,26 @@
-def group_by(f,iter):
+"""
+This module provides a function to group elements of an iterable based on a function.
+"""
+
+def group_by(f, iterable):
     """
-    Makes a dictionary where the keys are the results of applying f to each element in iter,
+    Makes a dictionary where the keys are the results of applying f to each element in iterable,
     and the values are lists of elements that produced that key.
-    Param f: function to apply to each element
-    Param iter: some iterable
-    Return a dictionary with keys as the results of f and values as lists of elements
+
+    Parameters:
+    f (function): Function to apply to each element.
+    iterable (iterable): The iterable to group.
+
+    Returns:
+    dict: A dictionary with keys as the results of f and values as lists of elements.
     """
-    return {k: [x for x in iter if f(x) == k] for k in {f(x) for x in iter}}
+    return {k: [x for x in iterable if f(x) == k] for k in {f(x) for x in iterable}}
+
 def main():
+    """
+    Main function to demonstrate the usage of the group_by function.
+    """
     print(group_by(len, ["hi", "bye", "yo", "try"]))
+
 if __name__ == '__main__':
     main()
-
