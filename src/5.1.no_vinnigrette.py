a57d535bd10791e75edbe643ef8863337c2335ce
eliran17e <eliran17eli@gmail.com>
2025-03-30 20:34:12 +0300

    Update 5.1.no_vinnigrette.py

--- src/5.1.no_vinnigrette.py
+++ src/5.1.no_vinnigrette.py
@@ -1,19 +1,20 @@
 import datetime
 import random
 
-def no_vinnigrete(date1,date2):
+"""
+This module provides a function to generate a random date between two dates
+and check if it is a Monday.
+"""
+
+def no_vinnigrete(date1, date2):
     """
     Generate a random date between two dates, regardless of their order
     and checks if its monday.
-
     Parameters:
     date1 (datetime or date): One of the boundary dates
     date2 (datetime or date): The other boundary date
-
     Returns true if its monday, false otherwise
     """
-
-
     if isinstance(date1, str):
         date1 = datetime.datetime.strptime(date1, "%Y-%m-%d")
     if isinstance(date2, str):
@@ -24,11 +25,14 @@ def no_vinnigrete(date1,date2):
     delta = (end_date - start_date).days
     random_days = random.randint(0, delta)
     date_result = start_date + datetime.timedelta(days=random_days)
-    if (date_result.weekday() == 0):
+    if date_result.weekday() == 0:
         print("Ain't gettin' no vinaigrette today :(")
 
-
 def main():
+    """
+    Main function to prompt user for two dates and check if a random date
+    between them is a Monday.
+    """
     dates = []
     while True:
         user_input = input("Please enter a date (YYYY-MM-DD): ")
@@ -39,5 +43,6 @@ def main():
         if len(dates) == 2:
             break
     no_vinnigrete(dates[0], dates[1])
+
 if __name__ == "__main__":
     main()
