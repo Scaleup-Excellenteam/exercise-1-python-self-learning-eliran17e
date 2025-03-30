d609b6305c9e8e17fdaa7f6c0fe0c61d69632fe8
eliran17e <eliran17eli@gmail.com>
2025-03-30 18:31:47 +0300

    Update 5.2.cup_of_join.py

--- src/5.2.cup_of_join.py
+++ src/5.2.cup_of_join.py
@@ -11,12 +11,13 @@ def cup_of_join(*lists, sep=None):
     """
     combined_list = []
     for index, item in enumerate(lists):
-        combined_list.extend(item)
+        if item:  
+            combined_list.extend(item)
         if index != len(lists) - 1 and sep is not None:
             combined_list.append(sep)
 
+    combined_list.append(sep)
     return combined_list
-
 
 
 def main():
