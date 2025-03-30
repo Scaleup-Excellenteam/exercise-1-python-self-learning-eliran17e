a87367d95b55fbd64bb32b9892080a2f88887d79
eliran17e <eliran17eli@gmail.com>
2025-03-30 16:21:44 +0300

    remember remember

---
+++ src/6.4.remember_remember.py
@@ -0,0 +1,45 @@
+import numpy as np
+from PIL import Image
+import os
+
+def is_black(pixel, tolerance=10):
+    """
+
+    Param pixel:
+    Param tolerance:
+    Return a boolean indicating if the pixel is black
+
+    """
+    return np.all(pixel <= tolerance)
+
+def decode_message_np(image_path):
+    """
+    Decode a message from an image using numpy.
+    Param image_path:
+    Return the decoded message as a string
+    """
+    img = Image.open(image_path).convert('RGB')
+    data = np.array(img)
+    height, width, _ = data.shape
+
+    message = ""
+    for x in range(width):
+        column = data[:, x]
+        black_rows = np.where([is_black(pixel) for pixel in column])[0]
+        if black_rows.size > 0:
+            black_row = black_rows[0]
+            message += chr(black_row)
+
+    return message
+
+def main():
+    relative_path = "./code.png"
+    full_path = os.path.abspath(relative_path)
+    if not os.path.exists(full_path):
+        print(f"File '{full_path}' not found.")
+        return
+    message = decode_message_np(full_path)
+    print("Decoded message:", message)
+
+if __name__ == '__main__':
+    main()
