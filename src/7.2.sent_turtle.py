9114ed661cf9cdd38f82f1261efeb35ed4b00edb
eliran17e <eliran17eli@gmail.com>
2025-03-30 22:30:44 +0300

    Update 7.2.sent_turtle.py

--- src/7.2.sent_turtle.py
+++ src/7.2.sent_turtle.py
@@ -1,7 +1,3 @@
-"""
-This module provides a Post Office class that allows users to message each other.
-"""
-
 class PostOffice:
     """A Post Office class. Allows users to message each other.
     Args:
