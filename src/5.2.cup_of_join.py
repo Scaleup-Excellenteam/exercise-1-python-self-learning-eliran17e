fd2a2140d4583beef2ac09e22a59ab9915f852cd
eliran17e <eliran17eli@gmail.com>
2025-03-30 21:42:27 +0300

    Update 5.2.cup_of_join.py

--- src/5.2.cup_of_join.py
+++ src/5.2.cup_of_join.py
@@ -1,5 +1,8 @@
-def cup_of_join(*lists, sep=None):
+"""
+This module provides a function to combine multiple lists into one list with a separator between them.
+"""
 
+def cup_of_join(*lists, sep=None):
     """
     Combine multiple lists into one list with a separator between them.
 
@@ -7,11 +10,12 @@ def cup_of_join(*lists, sep=None):
     lists (list): The lists to be combined
     sep (str): The separator to be used
 
-    Returns a list with the separator between them
+    Returns:
+    list: A list with the separator between them
     """
     combined_list = []
     for index, item in enumerate(lists):
-        if item:  
+        if item:
             combined_list.extend(item)
         if index != len(lists) - 1 and sep is not None:
             combined_list.append(sep)
@@ -19,12 +23,14 @@ def cup_of_join(*lists, sep=None):
         combined_list.append(sep)
     return combined_list
 
-
 def main():
+    """
+    Main function to demonstrate the usage of the cup_of_join function.
+    """
     list1 = [1, 2, 3]
     list2 = ['a', 'b', 'c']
     list3 = ['x', 'y', 'z']
-    print(join(list1, list2, list3, sep='@'))
+    print(cup_of_join(list1, list2, list3, sep='@'))
 
 if __name__ == '__main__':
     main()
