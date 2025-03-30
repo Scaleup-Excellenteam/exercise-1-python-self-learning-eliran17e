79a25cbaa62db5cb68ad1c95878fcad4a2afad46
eliran17e <eliran17eli@gmail.com>
2025-03-30 22:35:30 +0300

    Update 7.2.sent_turtle.py

--- src/7.2.sent_turtle.py
+++ src/7.2.sent_turtle.py
@@ -11,12 +11,13 @@ class PostOffice:
         self.message_id = 0
         self.boxes = {user: [] for user in usernames}
 
-    def send_message(self, sender, recipient, message, urgent=False):
+    def send_message(self, sender, recipient, title, message_body, urgent=False):
         """Send a message to a recipient.
         Args:
             sender (str): The message sender's username.
             recipient (str): The message recipient's username.
-            message (dict): The message details with 'title' and 'body'.
+            title (str): The title of the message.
+            message_body (str): The body of the message.
             urgent (bool, optional): The urgency of the message.
                                     Urgent messages appear first.
         Returns:
@@ -31,8 +32,8 @@ class PostOffice:
         self.message_id += 1
         message_details = {
             'id': self.message_id,
-            'title': message['title'],
-            'body': message['body'],
+            'title': title,
+            'body': message_body,
             'sender': sender,
             'unread': True,
         }
